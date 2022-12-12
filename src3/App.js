92490ba33674c1b6fe148311d4e3ed1c3761935f
unknown <moemensaadeh936@gmail.com>
2022-12-12 21:42:55 +0300

    new sample project

---
+++ src3/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src3/components/UI/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
+};
+
+export default Card;
