4421eff3eaf0a9f768394142f7244b630a32a70d
moemen saadeh <moemensaadeh936@gmail.com>
2023-11-21 02:05:33 +0300

    tailwind

---
+++ react-course-v3-main/10-tailwind-portfolio/my-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/hero";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Projects from "./components/Projects";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
+  );
+};
+
+export default App;
---
+++ react-course-v3-main/10-tailwind-portfolio/my-project/src/components/Skills.jsx
@@ -0,0 +1,17 @@
+import { skills } from "../data";
+import SectionTitle from "./SectionTitle";
+import SkillsCard from "./SkillsCard";
+const Skills = () => {
+  return (
+    <section className="py-20 align-element" id="skills">
+      <SectionTitle text="tech stack" />
+      <div className="py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3 first-letter">
+        {skills.map((skill) => {
+          return <SkillsCard key={skill.id} {...skill} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
